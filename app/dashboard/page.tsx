d15d1b54f57502286bb3274e69eec2e4e1c93aa0
Ng Chee Siang277275 <ncs000304@gmail.com>
2025-02-22 05:42:53 +0800

    feat: dashboard chart card skeleton

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,17 @@
+import { Card } from "@/components/ui/card";
+
 export default function Dashboard() {
   return (
-    <div className="max-width padding">
-      <h1>HelloDashboard World</h1>
+    <div className="max-width padding grid grid-cols-1 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <Card className="padding"></Card>
+        <Card className="padding"></Card>
+        <Card className="padding md:col-span-2 lg:col-span-1"></Card>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <Card className="padding"></Card>
+        <Card className="padding"></Card>
+      </div>
     </div>
   );
 }
