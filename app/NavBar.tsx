716f0d0d0799524f75832936950fa345d96a2fe7
Ng Chee Siang277275 <ncs000304@gmail.com>
2025-02-22 11:55:56 +0800

    style: nav bar styling

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,23 +1,38 @@
 "use client";
-import Link from "next/link";
 
-const route = [
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const routes = [
   { path: "/", pageTitle: "Home" },
   { path: "/dashboard", pageTitle: "Dashboard" },
 ];
 
 const NavBar = () => {
+  const pathname = usePathname();
+
   return (
     <nav className="padding flexbox-center">
-      <div className="flex items-center gap-4">
-        {route.map((item) => {
-          return (
-            <Link key={item.pageTitle} href={item.path}>
-              {item.pageTitle}
-            </Link>
-          );
-        })}
-      </div>
+      <Tabs
+        defaultValue={pathname}
+        value={pathname}
+        className="w-[400px] bg-secondary-color rounded-full p-2"
+      >
+        <TabsList className="grid w-full grid-cols-2">
+          {routes.map((item) => (
+            <TabsTrigger
+              key={item.pageTitle}
+              value={item.path}
+              className="shadow-none rounded-full"
+            >
+              <Link className="w-full" href={item.path}>
+                {item.pageTitle}
+              </Link>
+            </TabsTrigger>
+          ))}
+        </TabsList>
+      </Tabs>
     </nav>
   );
 };
