4246775a313f56c312a1691e5ae1e71781856e80
Ng Chee Siang277275 <ncs000304@gmail.com>
2025-02-22 14:51:07 +0800

    feat: add loading screen and skeleton component

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,8 +1,19 @@
+import { SkeletonChart } from "@/components/charts/SkeletonChart";
+
 const Loading = () => {
   return (
     <div className="parent-grid">
-      <div className="child-first-grid"></div>
-      <div className="child-second-grid"></div>
+      <div className="child-first-grid">
+        <SkeletonChart></SkeletonChart>
+        <SkeletonChart></SkeletonChart>
+        <div className="col-span-2 lg:col-span-1">
+          <SkeletonChart></SkeletonChart>
+        </div>
+      </div>
+      <div className="child-second-grid">
+        <SkeletonChart></SkeletonChart>
+        <SkeletonChart></SkeletonChart>
+      </div>
     </div>
   );
 };
---
+++ components/charts/SkeletonChart.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Skeleton } from "../ui/skeleton";
+
+export function SkeletonChart() {
+  return (
+    <Card className="pt-6">
+      <CardContent className="flex-1 pb-0">
+        <div className="mx-auto aspect-square space-y-4">
+          <Skeleton className="w-1/2 h-4 rounded-lg"></Skeleton>
+          <Skeleton className="w-full h-4 rounded-lg"></Skeleton>
+          <Skeleton className="w-full h-2/3 rounded-lg"></Skeleton>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
