9762cef0bc4a2bdfafacf4d3edcd3f6707363ac8
Ng Chee Siang277275 <ncs000304@gmail.com>
2025-02-23 11:59:51 +0800

    style: stat grid responsiveness

--- components/charts/StatCardGroup.tsx
+++ components/charts/StatCardGroup.tsx
@@ -15,7 +15,7 @@ interface StatCardGroupProps {
 
 export function StatCardGroup({ data }: StatCardGroupProps) {
   return (
-    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-5">
+    <div className="grid gap-4 sm:grid-cols-3 lg:grid-cols-5">
       {data.map((item, index) => (
         <Card key={index}>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
